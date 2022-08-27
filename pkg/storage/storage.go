4628f953bcdb09061f331ff54fdbda3cd9f58cca
Alexey Kirinyuk <akirinyuk@ozon.ru>
2022-08-27 16:09:03 +0300

    v2 fix

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -2,6 +2,6 @@ package storage
 
 import "github.com/alexeykirinyuk/storage/v2/internal/storage"
 
-func NewStorage() *storage.Storage {
+func NewStorage(newArg string) *storage.Storage {
 	return storage.NewStorage()
 }
